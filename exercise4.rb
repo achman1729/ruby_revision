fb881f3f038e5a1b464474a959cbcc06a47256d8
achman1729 <achin.madot@gmail.com>
2020-04-06 11:41:45 +1000

    exercise4 done

---
+++ exercise4.rb
@@ -0,0 +1,14 @@
+# Write a method that takes one argument, a number, and that returns true if the number is divisible by 5, and false otherwise.
+
+def div_by_5(number)
+    if number%5 ==0
+        return true
+    else
+        return false
+end
+
+
+for i in 1..20 do
+    p div_by_5(i)    
+end
+
