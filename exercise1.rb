cc0570e58e05e851aa3844cf44e03f31e47a7fc3
achman1729 <achin.madot@gmail.com>
2020-04-06 10:39:55 +1000

    exercise1 done

---
+++ exercise1.rb
@@ -0,0 +1,9 @@
+# Create an array of strings (with five elements). Make a loop from this array, and inside that loop concatenate the string with the string “balloon”, and print this to output.
+
+arr = [1, 2, 3, 4, 5]
+
+# puts "a" + "balloon"
+
+arr.each do |items|
+    p items.to_s + "balloon"
+end
